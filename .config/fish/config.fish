26f518f6b1558d7177666b5c23b892c01bde001e
Noran Raskin <n.raskin@campus.tu-berlin.de>
2021-12-05 18:01:23 +0100

    Add path with python3 symlink to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path /usr/local/bin/
 end
 
 # >>> conda initialize >>>
@@ -9,9 +10,3 @@ eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | so
 
 # Initialize google-cloud-sdk
 source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-# Add all python versions to path
-fish_add_path /usr/local/opt/python@3.7/bin
-fish_add_path /usr/local/opt/python@3.8/bin
-fish_add_path /usr/local/opt/python@3.9/bin
-fish_add_path /usr/local/opt/python@3.10/bin
-fish_add_path /usr/local/sbin
