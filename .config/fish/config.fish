60acb444aa6bb7f91a7cda424070422e81c5c907
Noran Raskin <n.raskin@campus.tu-berlin.de>
2022-04-22 21:00:39 +0200

    Add homebrew paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/sbin
     fish_add_path /opt/homebrew/opt/openjdk/bin
+    fish_add_path /opt/homebrew/opt/openjdk@17/include
 end
 
-brew
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
